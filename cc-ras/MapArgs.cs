5427a5b702d18483348ab11ee2b168792d6666f6
Jack <jackmschonherr@gmail.com>
2024-12-05 12:15:57 -0800

    initial map output

---
+++ cc-ras/MapArgs.cs
@@ -0,0 +1,90 @@
+﻿using Geospatial.GDALAssist;
+using Geospatial.IO;
+using Geospatial.Rasters;
+using Ras.Layers;
+using Ras.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utility.Extensions;
+using Utility.Progress;
+
+namespace cc_ras
+{
+    public class MapArgs
+    {
+        public string? ResultFilename { get; set; }
+        public string? OutputFilename { get; set; }
+        public string? TerrainFilename { get; set; }
+        public double CellSize { get; set; }
+        public int PfIdx { get; set; }
+        public MapTypes MapType { get; set; }
+
+        public bool Execute()
+        {
+            GDALSetup.InitializeMultiplatform("C:\\GDAL");
+
+            bool success;
+
+            Result result = new(ResultFilename);
+
+            success = GetTerrainAndProjection(out Projection srcPrj, out IResample<float> terr);
+            if (!success)
+                return false;
+
+
+            var res = new ResultMapResampler(MapType, result.BaseOutputBlock, terr, result.Geometry.FlowAreaLayer.AllPropertyTables());
+            IResample<float> band = res.AsSingleProfile(PfIdx);
+
+            Geospatial.Vectors.Extent ext = result.Geometry.HydraulicExtent;
+            RasterDefinition rdef = new(ext, CellSize);
+
+            ProgressReporter rep = new ConsoleProgressParser();
+            return WriteMap(srcPrj, band, rdef, rep);
+        }
+
+        /// <summary>
+        /// Writes the of a standard profile to file.
+        /// </summary>
+        private bool WriteMap(Projection outPrj, IResample<float> band, RasterDefinition rdef, ProgressReporter pr)
+        {
+            TiffExportEngine.ExportWithOverviews(OutputFilename, band, rdef, outPrj, pr);
+            return true;
+        }
+
+        /// <summary>
+        /// loads the terrain and projection from the file. tries to process as a ras terrain, tif, and finally with gdal reader. returns false
+        /// if all fail. 
+        /// </summary>
+        private bool GetTerrainAndProjection(out Projection srcPrj, out IResample<float> terr)
+        {
+            if (TerrainFilename.EndsWith(".h5") || TerrainFilename.EndsWith(".hdf"))
+            {
+                Terrain t = new(TerrainFilename);
+                srcPrj = t.TryGetProjection();
+                terr = t;
+                return true;
+            }
+            else if (TerrainFilename.EndsWith(".tif"))
+            {
+                // TODO: check int/short/etc
+                var ds = new TiffDataSource<float>(TerrainFilename);
+                terr = ds.AsRasterizer();
+                srcPrj = GDALRaster.GetProjection(TerrainFilename);
+                return true;
+            }
+            else
+            {
+                // TODO: Try gdal reader
+                srcPrj = null;
+                terr = null;
+                Console.WriteLine("Unknown File/Extension: " + TerrainFilename.InDoubleQuotes());
+                return false;
+            }
+        }
+    }
+
+    
+}
--- cc-ras/Program.cs
+++ cc-ras/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Geospatial.IO;
+using Geospatial.Rasters;
+using Ras.Layers;
+using Ras.Mapping;
+using Usace.CC.Plugin;
+using Utility.Progress;
+using Geospatial.GDALAssist;
+using Utility.Extensions;
+using System.Data.SQLite;
+using cc_ras;
+
+class Test
+{
+    private static void SetEnv(string name, string value)
+    {
+        Environment.SetEnvironmentVariable(name, value, EnvironmentVariableTarget.Process);
+    }
+
+    public static void Main()
+    {
+        //SetEnv("CC" + "_" + EnvironmentVariables.AWS_S3_BUCKET, "test-bucket-1");
+        //SetEnv("CC" + "_" + EnvironmentVariables.AWS_ACCESS_KEY_ID, "USERNAME");
+        //SetEnv("CC" + "_" + EnvironmentVariables.AWS_SECRET_ACCESS_KEY, "PASSWORD");
+        //SetEnv("CC" + "_" + "S3_MOCK", "true");
+        //SetEnv("CC" + "_" + "S3_ENDPOINT", "http://127.0.0.1:9000");
+
+        //var bucket = new AwsBucket("CC");
+        //await bucket.CreateBucketIfNotExists();
+
+        //string key = "MINIO-test.txt";
+        //bool exists = await bucket.ObjectExists(key);
+        //if (!exists)
+        //{
+        //    throw new Exception("object not found");
+        //}
+        //string input = await bucket.ReadObjectAsText(key);
+        //Console.WriteLine(input);
+
+        //string compute = input + " and this was appended";
+        //string computeKey = "MINIO-test-compute.txt";
+        //await bucket.CreateObject(computeKey, compute);
+
+        //string result = await bucket.ReadObjectAsText(computeKey);
+        //Console.WriteLine(result);
+
+        //PluginManager pm = await PluginManager.CreateAsync();
+        //Payload payload = pm.Payload;
+
+        MapArgs mapArgs = new MapArgs();
+        mapArgs.ResultFilename = "C:\\Users\\HEC\\Documents\\RAS Projects\\Berryessa 2025 Dambreak\\Results\\Dam Break.h5";
+        mapArgs.TerrainFilename = "C:\\Users\\HEC\\Documents\\RAS Projects\\Berryessa 2025 Dambreak\\Terrains\\Terrain.h5";
+        mapArgs.OutputFilename = "C:\\Users\\HEC\\source\\repos\\cc-ras\\output.tif";
+        mapArgs.CellSize = 10;
+        mapArgs.PfIdx = 5;
+        mapArgs.MapType = MapTypes.Depth;
+
+        mapArgs.Execute();
+
+    }
+
+
+    
+}
