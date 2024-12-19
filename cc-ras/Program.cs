cb6421951d8dd5c3d88946f94c51093a5f7fd43c
Jack <jackmschonherr@gmail.com>
2024-12-18 16:04:58 -0800

    new minio testing container

--- cc-ras/Program.cs
+++ cc-ras/Program.cs
@@ -22,7 +22,7 @@ class Test
         SetEnv(EnvironmentVariables.CC_MANIFEST_ID, "1");
         SetEnv(EnvironmentVariables.CC_EVENT_NUMBER, "987");
         SetEnv(EnvironmentVariables.CC_EVENT_ID, "57");
-        SetEnv(EnvironmentVariables.CC_ROOT, "data");
+        SetEnv(EnvironmentVariables.CC_ROOT, "cc_store");
         SetEnv(EnvironmentVariables.CC_PLUGIN_DEFINITION, "ras");
         SetEnv(EnvironmentVariables.CC_PROFILE, "CC");
         SetEnv(EnvironmentVariables.CC_PROFILE + "_" + EnvironmentVariables.AWS_S3_BUCKET, "cc-bucket");
@@ -36,20 +36,20 @@ class Test
         */
 
 
-        PluginManager pm = await PluginManager.CreateAsync();
-        pm.LogMessage("hello from RAS Plugin");
+        //PluginManager pm = await PluginManager.CreateAsync();
+        //pm.LogMessage("hello from RAS Plugin");
 
-        Payload p = pm.Payload;
+        //Payload p = pm.Payload;
         
-        MapArgs mapArgs = new MapArgs();
-        mapArgs.ResultFilename = pm.getInputDataSource("Result File").Paths[0];
-        mapArgs.TerrainFilename = pm.getInputDataSource("Terrain File").Paths[0];
-        mapArgs.OutputFilename = pm.getOutputDataSource("Output File").Paths[0];
-        mapArgs.CellSize = int.Parse(p.Attributes["CellSize"]);
-        mapArgs.PfIdx = int.Parse(p.Attributes["PfIndx"]);
-        mapArgs.MapType = (MapTypes)Enum.Parse(typeof(MapTypes), p.Attributes["MapType"]);
+        //MapArgs mapArgs = new MapArgs();
+        //mapArgs.ResultFilename = pm.getInputDataSource("Result File").Paths[0];
+        //mapArgs.TerrainFilename = pm.getInputDataSource("Terrain File").Paths[0];
+        //mapArgs.OutputFilename = pm.getOutputDataSource("Output File").Paths[0];
+        //mapArgs.CellSize = int.Parse(p.Attributes["CellSize"]);
+        //mapArgs.PfIdx = int.Parse(p.Attributes["PfIndx"]);
+        //mapArgs.MapType = (MapTypes)Enum.Parse(typeof(MapTypes), p.Attributes["MapType"]);
 
-        mapArgs.Execute();
+        //mapArgs.Execute();
 
     }
 }
